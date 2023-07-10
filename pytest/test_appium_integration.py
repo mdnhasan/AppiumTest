3369faa63a83b409c48a9cb82320638d305cae2d
nasim <mdnasimhasanbd@gmail.com>
2023-07-10 16:58:46 +0600

    Html&Allure Report

--- pytest/test_appium_integration.py
+++ pytest/test_appium_integration.py
@@ -47,6 +47,9 @@ def getData():
 
 """Lesson learn 'Never use name change'"""
 
+"""setup (For run once in one test case if two value like dubai and delhi)"""
+"""setup_function (For run times need in one test case if two or more value like dubai and delhi)"""
+
 
 def setup_function():
     global driver
@@ -77,65 +80,56 @@ def test_login(city):
     driver.find_elements(By.ID, "com.goibibo:id/lytChildNode1")[0].click()
     time.sleep(4)
     driver.find_element(By.ID, "com.goibibo:id/verticalHomeSearchButton").click()
-    time.sleep(3)
+    time.sleep(5)
     citytext = driver.find_element(By.XPATH, "//android.widget.TextView[contains(@text, 'Top Areas to Stay')]").text
     print(citytext)
-    newcitytext = str(citytext).replace("Top ", "").replace("Stay", "")
-    print(newcitytext)
-    assert newcitytext in citytext, "Contain Found"
-
-
-
-
 
 #######Code From Lecture##############
-    #
-    # import time
-    #
-    # import pytest
-    # from appium import webdriver
-    # from appium.webdriver.appium_service import AppiumService
-    #
-    # def get_data():
-    #     return [
-    #
-    #         ["Delhi"],
-    #         ["Dubai"],
-    #
-    #     ]
-    #
-    # def setup_function():
-    #     global appium_service
-    #     appium_service = AppiumService()
-    #     appium_service.start()
-    #
-    #     desired_caps = {}
-    #     desired_caps['platformName'] = 'Android'
-    #     desired_caps['deviceName'] = 'Android'
-    #     desired_caps['appPackage'] = 'com.goibibo'
-    #     desired_caps['appActivity'] = '.common.HomeActivity'
-    #     desired_caps['noReset'] = True
-    #     global driver
-    #     driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
-    #     driver.implicitly_wait(10)
-    #
-    # def teardown_function():
-    #     time.sleep(2)
-    #     driver.quit()
-    #     appium_service.stop()
-    #
-    # @pytest.mark.parametrize("city", get_data())
-    # def test_dologin(city):
-    #     driver.find_element_by_id('com.goibibo:id/btn1').click()
-    #     driver.find_element_by_accessibility_id('destination').click()
-    #     driver.find_element_by_id('com.goibibo:id/edtSearch').send_keys(city)
-    #     driver.find_elements_by_id('com.goibibo:id/lytLocationItem')[0].click()
-    #     driver.find_element_by_xpath("//android.widget.TextView[@text='Search']").click()
-    #     cityText = driver.find_element_by_xpath("//android.widget.TextView[contains(@text,'EXPLORE')]").text
-    #     print(cityText)
-    #     newCityText = str(cityText).replace("EXPLORE ", "").replace("!", "")
-    #     print(newCityText)
-    #
-    #     assert newCityText in str(city).upper()
-
-
+#
+# import time
+#
+# import pytest
+# from appium import webdriver
+# from appium.webdriver.appium_service import AppiumService
+#
+# def get_data():
+#     return [
+#
+#         ["Delhi"],
+#         ["Dubai"],
+#
+#     ]
+#
+# def setup_function():
+#     global appium_service
+#     appium_service = AppiumService()
+#     appium_service.start()
+#
+#     desired_caps = {}
+#     desired_caps['platformName'] = 'Android'
+#     desired_caps['deviceName'] = 'Android'
+#     desired_caps['appPackage'] = 'com.goibibo'
+#     desired_caps['appActivity'] = '.common.HomeActivity'
+#     desired_caps['noReset'] = True
+#     global driver
+#     driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+#     driver.implicitly_wait(10)
+#
+# def teardown_function():
+#     time.sleep(2)
+#     driver.quit()
+#     appium_service.stop()
+#
+# @pytest.mark.parametrize("city", get_data())
+# def test_dologin(city):
+#     driver.find_element_by_id('com.goibibo:id/btn1').click()
+#     driver.find_element_by_accessibility_id('destination').click()
+#     driver.find_element_by_id('com.goibibo:id/edtSearch').send_keys(city)
+#     driver.find_elements_by_id('com.goibibo:id/lytLocationItem')[0].click()
+#     driver.find_element_by_xpath("//android.widget.TextView[@text='Search']").click()
+#     cityText = driver.find_element_by_xpath("//android.widget.TextView[contains(@text,'EXPLORE')]").text
+#     print(cityText)
+#     newCityText = str(cityText).replace("EXPLORE ", "").replace("!", "")
+#     print(newCityText)
+#
+#     assert newCityText in str(city).upper()
